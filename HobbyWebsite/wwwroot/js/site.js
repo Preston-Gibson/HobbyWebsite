c15183a9a37503151861beb0a646e4deb9dc1136
Preston <prestong378@gmail.com>
2026-02-07 11:11:19 -0700

    Adding icon and fixing js

--- HobbyWebsite/wwwroot/js/site.js
+++ HobbyWebsite/wwwroot/js/site.js
@@ -1,7 +1,6 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
 function calculateFee() {
     let hourlyRate = $('#hourlyRate').val();
     let numHours = $('#numHours').val();
