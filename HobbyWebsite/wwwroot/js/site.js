c00f4cca8d59f08b7e0e3a818114dabe5ec7c23e
Preston <prestong378@gmail.com>
2026-02-07 10:46:05 -0700

    Calculator and formatting

--- HobbyWebsite/wwwroot/js/site.js
+++ HobbyWebsite/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function calculateFee() {
+    let hourlyRate = $('#hourlyRate').val();
+    let numHours = $('#numHours').val();
+    if (numHours >= 0 ) {
+        $('#resultString').text(`Total: $${(hourlyRate * numHours).toFixed(2)}`)
+    } else {
+        alert(`The number of hours must be a positive number.`);
+    }
+}
